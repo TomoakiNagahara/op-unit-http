68faacf17c55207d9507ee3e4cdc73b8eb103785
Tomoaki mbp13r-1 <tomoaki.nagahara@gmail.com>
2019-03-25 09:12:49 +0900

    New: index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * unit-http:/index.php
+ *
+ * @creation  2019-03-24
+ * @version   1.0
+ * @package   unit-http
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+//	...
+include(__DIR__.'/Http.class.php');
